3d33a17fb60689436b372f1ac8241865054d6b16
Steve Meisner <steve@meisners.net>
2025-03-13 15:18:32 -0400

    Add definitions for projector HW

--- src/hwdef.h
+++ src/hwdef.h
@@ -48,6 +48,8 @@
 
 #ifdef HUZZAH32
 
+  #define USE_PROJECTOR
+
   #define PIN_BUILTIN_LED 13
   #define BUZZZER_PIN 18
   #define TRIGGER_AP_PIN 4
@@ -62,6 +64,18 @@
   #define SNOOZE_PIN_PLUS 19
   #define SNOOZE_PIN_MINUS 23
 
+  #ifdef TYPE_TM1621D
+    #define PIN_PROJECTOR_CS     15
+    #define PIN_PROJECTOR_CLK    32
+    #define PIN_PROJECTOR_DATA   14
+  #endif
+
+  #ifdef TYPE_ET6621S
+    #define PIN_PROJECTOR_CS     14
+    #define PIN_PROJECTOR_CLK    32
+    #define PIN_PROJECTOR_DATA   15
+  #endif
+
   // i2c protocol support
   #define ESP32_SCL 22
   #define ESP32_SDA 21
@@ -74,19 +88,25 @@
 
 #ifdef DEXCOM_PCB
 
-  #define PIN_BUILTIN_LED 13
-  #define BUZZZER_PIN 26
-  #define TRIGGER_AP_PIN 0//44  44 is a pin that will never trigger...0 is the boot switch
+  #define USE_PROJECTOR
+
+  #define PIN_BUILTIN_LED      13
+  #define BUZZZER_PIN          26
+  #define TRIGGER_AP_PIN       0 //44  44 is a pin that will never trigger...0 is the boot switch
 
   // Populated buttons
-  #define SELECT_PIN 33
-  #define BACK_PIN 12
-  #define RIGHT_PIN 42
-  #define LEFT_PIN 14
-  #define UP_PIN 19
-  #define DOWN_PIN 41
-  #define SNOOZE_PIN_PLUS 1
-  #define SNOOZE_PIN_MINUS 2
+  #define SELECT_PIN           33
+  #define BACK_PIN             12
+  #define RIGHT_PIN            42
+  #define LEFT_PIN             14
+  #define UP_PIN               19
+  #define DOWN_PIN             41
+  #define SNOOZE_PIN_PLUS      1
+  #define SNOOZE_PIN_MINUS     2
+
+  #define PIN_PROJECTOR_CS     15
+  #define PIN_PROJECTOR_CLK    6
+  #define PIN_PROJECTOR_DATA   7
 
   // i2c protocol support
   #define ESP32_SCL 3
@@ -95,6 +115,10 @@
   // This board removes the RGB LED
   #define NO_RGBLED
 
+  // ...and adds a 2.42" OLED and a TC74 temp sensor
+  #define OLED_SSD1306_I2C_ADDRESS 0x3D
+  #define TC74_I2C_ADDRESS 0x48
+  
 #endif
 
 // If we have an RGB LED, be sure to indicate the pins used
@@ -102,4 +126,20 @@
   #define R_PIN 17
   #define G_PIN 16
   #define B_PIN 5
+#endif
+
+#ifdef USE_PROJECTOR
+  //
+  // Projector related details
+  //
+
+  //
+  // RTOS task to update the Projector. Located
+  // in projector.cpp and called from main.cpp.
+  //
+  void projectorUpdateTask();
+
+  // Correction used for onboard temp sensor (to indicate ambient room temp)
+  #define TEMP_CORRECTION 0
+
 #endif
